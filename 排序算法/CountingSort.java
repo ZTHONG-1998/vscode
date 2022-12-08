aa258c549f2cd167b5f596f95565908ee643b06d
zt_hong <120079547+ZTHONG-1998@users.noreply.github.com>
2022-12-08 14:20:58 +0800

    commit

--- 排序算法/CountingSort.java
+++ 排序算法/CountingSort.java
@@ -1,5 +1,49 @@
-public class CountSort {
+public class CountingSort {
+    // 伪计数排序
+    public void countingSort(int[] nums) {
+        if (nums == null || nums.length == 0) return;
+        int minValue = nums[0], maxValue = nums[0];
+        for (int x : nums) {
+            if (x > maxValue) maxValue = x;
+            else if (x < minValue) minValue = x;
+        }
+        int[] cnt = new int[maxValue - minValue + 1];
+        for (int x : nums) {
+            cnt[x - minValue]++;
+        }
+        for (int i = 0, j = 0; i < cnt.length; i++) {
+            while (cnt[i] > 0) {
+                nums[j++] = i + minValue;
+                cnt[i]--;
+            }
+        }
+    }
+
+    // 真正的计数排序
+    public void countingSort(int[] nums) {
+        if (nums == null || nums.length == 0) return;
+        int minValue = nums[0], maxValue = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > maxValue) maxValue = nums[i];
+            else if (nums[i] < minValue) minValue = nums[i];
+        }
+        int[] cnt = new int[maxValue - minValue + 1];
+        for (int x : nums) {
+            cnt[x - minValue]++;
+        }
+        for (int i = 1; i < cnt.length; i++) {
+            cnt[i] += cnt[i - 1];
+        }
+        int[] res = new int[nums.length];
+        for (int i = nums.length - 1; i >= 0; i--) {
+            res[--cnt[nums[i] - minValue]]] = nums[i];
+        }
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = res[i];
+        }
+    }
+
     public static void main() {
-        
+
     } 
 }
